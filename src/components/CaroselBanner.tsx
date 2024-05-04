1e1e4f44ccdd7365a64ed95b835c2f62c36b42d5
shimaa <shimaamoftah152@gmail.com>
2024-05-04 21:27:52 +0300

    first commit

---
+++ src/components/CaroselBanner.tsx
@@ -0,0 +1,19 @@
+import { getDiscoverMovies } from "@/lib/getMovies";
+import HeroCarousel from "./HeroCarousel";
+
+interface Props {
+  id?: string;
+  keywords?: string;
+}
+
+const CaroselBanner = async ({ id, keywords }: Props) => {
+  const movies = await getDiscoverMovies(id, keywords);
+
+  return <>
+
+     <HeroCarousel movies={movies} />
+  </>
+  
+};
+
+export default CaroselBanner;
